f05118d566db7e8a36c89cebc7bc5ce956bef97c
Steve Barbaro <steve.barbaro@gmail.com>
2022-06-13 17:56:08 -0500

    adds Animations Page css page + wraps animations page in a flexbox

--- pages/animations.js
+++ pages/animations.js
@@ -1,11 +1,13 @@
 import Header from '../components/header.js'
 import ReactPlayer from "react-player"
+import styles from '../styles/Animations.module.css'
 
 export default function Animations() {
   return (
     <>
     <Header headerBackground="animationsBackground"/>
-    <div>Animations</div>
+    <div className={styles.videoContainer}>
+    <p>Animations</p>
     <ReactPlayer
          url="https://vimeo.com/203852613"
        />
@@ -14,6 +16,7 @@ export default function Animations() {
        <ReactPlayer
             url="https://vimeo.com/42249909"
           />
+      </div>
 
   </>
   )
