140e13b2127c5a83425752f389607ef4f9d5bbd0
Steve Barbaro <steve.barbaro@gmail.com>
2022-06-15 15:23:26 -0500

    solidifies base object structure for home page book data + injects all properly sized book jpegs + injects various other data

--- public/HomePageBookEntries.js
+++ public/HomePageBookEntries.js
@@ -1,10 +1,67 @@
 export const books = {
-  CLOAK: {
+  THENEWHOUSE: {
+    imgUrl: "/images/NewHouseCover.jpg",
+    title: "The New House",
+    url: "/newhouse",
+    thumbHeight: "420",
+    thumbWidth: "510",
+    height: "600",
+    width: "800",
+    projectUrl: "hhttps://whiskeytit.com/product/the-new-house/",
+    objectDetails:
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+    text: [
+      {
+        className: "italic",
+        text:
+          "The cloak is not so much a means of becoming invisible as it is an apparatus for concealing intentions. It is a shroud or a curtain. We can detect the movements occurring behind it, but remain unable to narrow what those movements might be, where they might lead, what they might catalyze.",
+      },
+      {
+        text:
+          "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+      },
+      {
+        text:
+          "CLOAK is not intended to fully encompass these relationships, but instead to instigate new connections / new contexts. We have assembled this work as a starting point. The first flourish of the stealth weapon / integumentary drape / vestigial skin.",
+      },
+    ],
+    shortText:
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+  },
+  CHILDRENOFTHENEWFLESH: {
+    imgUrl: "/images/ChildrenCover.jpg",
+    title: "Children of The New Flesh: The Early Work and Pervasive Influence of David Cronenberg",
+    url: "/children",
+    thumbHeight: "420",
+    thumbWidth: "510",
+    height: "600",
+    width: "800",
+    projectUrl: "https://1111press.bigcartel.com/product/children-of-the-new-flesh-preorder",
+    objectDetails:
+      "POLICE LUCIFER / William Watson / 44 pages / $8",
+    text: [
+      {
+        text:
+          "A digital conversation drifts on archaic CRT monitors. Basal AI composite-syntax duels itself in the spaces between Foucault's DISCIPLINE & PUNISH and Plato's FORMS. Humanity continues its de-evolution towards ambient stasis. Discoveries are rendered stillborn as the code of rhetoric is disabled."
+      },
+      {
+        text:
+          "Within the miasma, Edmund Emil Kemper III has arrived. The metaphysical independent variable. The surveillant of horrific violence and flexuous plasticene abjection."
+      },
+      {
+        text:
+          "Where does depravity cross condolence? Where does the illogical become sentimental?"
+      },
+    ],
+    shortText:
+      "POLICE LUCIFER // CLICK HERE TO DOWNLOAD ADDITIONAL AMORALITY DRIVERS"
+  },
+  DRIFTER: {
     imgUrl: "/images/DrifterCover.jpg",
     title: "Drifter: Stories",
-    url: "/catalogue/drifter",
-    thumbHeight: "670",
-    thumbWidth: "760",
+    url: "/drifter",
+    thumbHeight: "420",
+    thumbWidth: "510",
     height: "600",
     width: "800",
     projectUrl: "https://1111press.com/drifter-stories",
@@ -24,41 +81,124 @@ export const books = {
         text:
           "CLOAK is not intended to fully encompass these relationships, but instead to instigate new connections / new contexts. We have assembled this work as a starting point. The first flourish of the stealth weapon / integumentary drape / vestigial skin.",
       },
-      { text: "What shapes it can be contorted into are yet to be seen." },
     ],
     shortText:
       "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
   },
-  POLICELUCIFER: {
-    imgUrl: "/images/PL-Coverspread.png",
-    title: "POLICELUCIFER",
-    url: "catalogue/POLICELUCIFER",
-    thumbHeight: "270",
-    thumbWidth: "360",
+  ANGELHOUSE: {
+    imgUrl: "/images/AngelHouseCover.jpg",
+    title: "Angel House: A Novel",
+    url: "/angel",
+    thumbHeight: "420",
+    thumbWidth: "510",
     height: "600",
     width: "800",
-    projectUrl: "https://bookshop.org/books/police-lucifer/9780578285528",
+    projectUrl: "http://www.kernpunktpress.com/store/p17/angelhouse.html?fbclid=IwAR3cxvs1UuxYWa2LYpmLRljFGnXJiQWUlUO045nKhmi6pLjLmQj4sbW9cW4",
     objectDetails:
-      "POLICE LUCIFER / William Watson / 44 pages / $8",
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
     text: [
       {
+        className: "italic",
         text:
-          "A digital conversation drifts on archaic CRT monitors. Basal AI composite-syntax duels itself in the spaces between Foucault's DISCIPLINE & PUNISH and Plato's FORMS. Humanity continues its de-evolution towards ambient stasis. Discoveries are rendered stillborn as the code of rhetoric is disabled."
+          "The cloak is not so much a means of becoming invisible as it is an apparatus for concealing intentions. It is a shroud or a curtain. We can detect the movements occurring behind it, but remain unable to narrow what those movements might be, where they might lead, what they might catalyze.",
       },
       {
         text:
-          "Within the miasma, Edmund Emil Kemper III has arrived. The metaphysical independent variable. The surveillant of horrific violence and flexuous plasticene abjection."
+          "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
       },
       {
         text:
-          "Where does depravity cross condolence? Where does the illogical become sentimental?"
+          "CLOAK is not intended to fully encompass these relationships, but instead to instigate new connections / new contexts. We have assembled this work as a starting point. The first flourish of the stealth weapon / integumentary drape / vestigial skin.",
       },
-      {
-        text:
-          "POLICE LUCIFER is the rogue transmission you were not supposed to catch."
-      }
     ],
     shortText:
-      "POLICE LUCIFER // CLICK HERE TO DOWNLOAD ADDITIONAL AMORALITY DRIVERS"
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+  },
+  AROOMINDOGDECITY: {
+    imgUrl: "/images/DodgeCityCover.jpg",
+    title: "A Room in Dodge City: A Novel-in-Vignettes",
+    url: "/dodge",
+    thumbHeight: "420",
+    thumbWidth: "510",
+    height: "600",
+    width: "800",
+    projectUrl: "https://www.press.alternatingcurrentarts.com/2016/12/a-room-in-dodge-city-david-leo-rice.html",
+    objectDetails:
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+    text: [
+      {
+        className: "italic",
+        text:
+          "The cloak is not so much a means of becoming invisible as it is an apparatus for concealing intentions. It is a shroud or a curtain. We can detect the movements occurring behind it, but remain unable to narrow what those movements might be, where they might lead, what they might catalyze.",
+      },
+      {
+        text:
+          "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+      },
+      {
+        text:
+          "CLOAK is not intended to fully encompass these relationships, but instead to instigate new connections / new contexts. We have assembled this work as a starting point. The first flourish of the stealth weapon / integumentary drape / vestigial skin.",
+      },
+    ],
+    shortText:
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+  },
+  AROOMINDOGDECITYTWO: {
+    imgUrl: "/images/DodgeCityTwoCover.jpg",
+    title: "A Room in Dodge City Volume Two: The Blut Branson Era",
+    url: "/dodgetwo",
+    thumbHeight: "420",
+    thumbWidth: "510",
+    height: "600",
+    width: "800",
+    projectUrl: "https://www.press.alternatingcurrentarts.com/2020/08/a-room-in-dodge-city-2-david-leo-rice.html",
+    objectDetails:
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+    text: [
+      {
+        className: "italic",
+        text:
+          "The cloak is not so much a means of becoming invisible as it is an apparatus for concealing intentions. It is a shroud or a curtain. We can detect the movements occurring behind it, but remain unable to narrow what those movements might be, where they might lead, what they might catalyze.",
+      },
+      {
+        text:
+          "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+      },
+      {
+        text:
+          "CLOAK is not intended to fully encompass these relationships, but instead to instigate new connections / new contexts. We have assembled this work as a starting point. The first flourish of the stealth weapon / integumentary drape / vestigial skin.",
+      },
+    ],
+    shortText:
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+  },
+  THEPORNMETRILOGY: {
+    imgUrl: "/images/PornMeCover.jpg",
+    title: "The Pornme Trilogy",
+    url: "/pornme",
+    thumbHeight: "420",
+    thumbWidth: "510",
+    height: "600",
+    width: "800",
+    projectUrl: "https://publish.sweek.com/shop/index.php/catalog/product/view/id/479321/s/the-pornme-trinity-210679-publish-sweek-com/category/337/",
+    objectDetails:
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+    text: [
+      {
+        className: "italic",
+        text:
+          "The cloak is not so much a means of becoming invisible as it is an apparatus for concealing intentions. It is a shroud or a curtain. We can detect the movements occurring behind it, but remain unable to narrow what those movements might be, where they might lead, what they might catalyze.",
+      },
+      {
+        text:
+          "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
+      },
+      {
+        text:
+          "CLOAK is not intended to fully encompass these relationships, but instead to instigate new connections / new contexts. We have assembled this work as a starting point. The first flourish of the stealth weapon / integumentary drape / vestigial skin.",
+      },
+    ],
+    shortText:
+      "DRIFTER is a collection containing a decade's worth of short fiction by David Leo Rice, with work first published in 'The Fanzine', 'The Rumpus', 'Black Clock', 'DIAGRAM' and elsewhere",
   }
 };
