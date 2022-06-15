66fc86c3db0859e9ce2bd6da926dc09bdfdf9d5a
Steve Barbaro <steve.barbaro@gmail.com>
2022-06-14 22:13:36 -0500

    instantiates social-link box for Wake Island Page

--- pages/wake-island-podcast.js
+++ pages/wake-island-podcast.js
@@ -11,6 +11,10 @@ export default function WakeIsland() {
     <Header headerBackground="wakeIslandBackground"/>
     <p className={styles.title}>... Wake Island Podcast ...</p>
     <p className={styles.subtitle}>David Leo Rice cohosts this podcast about contemporary art and literature, transgression, escape, and the darkening undercurrents of American life in the 2020s).</p>
+    <div className={styles.socialBox}>
+    <a href="https://twitter.com/wakeislandpod?lang=en" target="_blank" rel="noreferrer">&#10239;twitter&#8668;</a>
+    <a href="https://www.instagram.com/wakeislandpod/?hl=en" target="_blank" rel="noreferrer">&#8668;instagram&#8668;</a>
+    </div>
     <div className={styles.wakeIslandContainer}>
     <iframe frameborder="0" height="450" width="600" src="https://embed.podcasts.apple.com/us/podcast/wake-island/id1485799907"></iframe>
     </div>
